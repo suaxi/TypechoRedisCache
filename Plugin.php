56cb55bf7cce6e843baac211b29286dcf1634887
suaxi <281463547@qq.com>
2025-04-08 21:19:40 +0800

    init project

---
+++ Plugin.php
@@ -0,0 +1,87 @@
+<?php
+
+if (!defined('__TYPECHO_ROOT_DIR__')) {
+    exit;
+}
+
+/**
+ * Redis Cache
+ *
+ * @package TypechoRedisCache
+ * @author suaxi
+ * @version 0.0.1 beta
+ * @link http://www.wangchouchou.com
+ */
+class TypechoRedisCache_Plugin implements Typecho_Plugin_Interface
+{
+    private static $redis;
+
+    public static function activate()
+    {
+        Typecho_Plugin::factory('Widget_Archive')->beforeRender = array('TypechoRedisCache_Plugin', 'cache');
+        Typecho_Plugin::factory('Widget_Contents_Post_Edit')->finishPublish = array('TypechoRedisCache_Plugin', 'clearCache');
+        Typecho_Plugin::factory('Widget_Contents_Post_Edit')->finishDelete = array('TypechoRedisCache_Plugin', 'clearCache');
+        return _t('RedisCache 插件已激活');
+    }
+
+    public static function deactivate()
+    {
+        return _t('RedisCache 插件已禁用');
+    }
+
+    public static function config(Typecho_Widget_Helper_Form $form)
+    {
+        $host = new Typecho_Widget_Helper_Form_Element_Text('host', NULL, '127.0.0.1', _t('Redis 服务器地址'));
+        $port = new Typecho_Widget_Helper_Form_Element_Text('port', NULL, '6379', _t('Redis 服务器端口'));
+        $dbNum = new Typecho_Widget_Helper_Form_Element_Text('dbNum', NULL, '0', _t('Redis 数据库(0-15)'));
+        $expire = new Typecho_Widget_Helper_Form_Element_Text('expire', NULL, '86400', _t('缓存过期时间（秒）'));
+        $form->addInput($host);
+        $form->addInput($port);
+        $form->addInput($dbNum);
+        $form->addInput($expire);
+    }
+
+    public static function personalConfig(Typecho_Widget_Helper_Form $form) {}
+
+    public static function cache($archive)
+    {
+        if ($archive->is('post')) {
+            self::connectRedisServer();
+            
+            $key = 'ARTICLE:' . md5($_SERVER['REQUEST_URI']);
+
+            $articleCache = self::$redis->get($key);
+
+            if ($articleCache) {
+                $archive = $articleCache;
+            } else {
+                self::$redis->set($key, $archive, Typecho_Widget::widget('Widget_Options')->plugin('TypechoRedisCache')->expire);
+            }
+        }
+    }
+
+    public static function clearCache($contents, $class)
+    {
+        self::connectRedisServer();
+        $key = 'ARTICLE:' . md5($contents['permalink']);
+        self::$redis->del($key);
+    }
+
+    private static function connectRedisServer()
+    {
+        if (!self::$redis) {
+            try
+            {
+                $options = Typecho_Widget::widget('Widget_Options')->plugin('TypechoRedisCache');
+                self::$redis = new Redis();
+                self::$redis->connect($options->host, $options->port);
+                self::$redis->select($options->dbNum);
+                return true;
+            }
+            catch(Exception $e)
+            {
+                throw new Exception("Redis服务端连接异常,请检查!");
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# TypechoRedisCache
-Typecho Redis Cache Plugin
+### Typecho Redis 文章缓存插件
+
+#### 使用说明：
+
+1. 安装 `redis-php`
+2. `php.ini ` 开启 `redis` 扩展
+3. 将插件上传至 `typecho/plugins` 插件目录，并在后台 - 插件管理 - 启用
+
+
+
+#### 功能：
+
+1. 文章缓存
+
+
+
+#### 开发计划：
+
+- [x] ~~缓存可视化界面~~，请使用 RDM、RedisInsight 等工具类
+
+- [ ] 按类别（文章标题、内容、分类、标签）缓存
+- [ ] Redis 服务端连接配置优化
